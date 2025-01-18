4e529ce8b962fb0d658747922dac08dfe087fda5
Jonas Pinheiro <jonasespinheiro@gmail.com>
2025-01-18 13:42:10 -0300

    Update Contact.tsx
    Adding buttons to contact me

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,8 +2,12 @@ const Contact = () => {
     return(
         <div>
             <h1>Contact</h1>
+            <button><a href="mailto:jonasespinheiro@gmail.com" target="_blank" rel='noopener'>E-mail</a></button>
+            <button><a href="https://github.com/jonasedpi" target="_blank" rel='noopener'>Github</a></button>
+            <button><a href="https://www.linkedin.com/in/jonas-eduardo-schumann-pinheiro/" target="_blank" rel='noopener'>Linkedin</a></button>
+            <button><a href="https://api.whatsapp.com/send?phone=5551999239099" target="_blank" rel='noopener'>WhatsApp</a></button>
         </div>
-    )
+    );
 }
 
 export default Contact
